cf6b480f909711bdbff9078925fa124dc1194032
Manu <52102823+Manu098vm@users.noreply.github.com>
2023-12-01 16:13:36 +0100

    Add MgdbForm

---
+++ SwitchGiftDataManager.WinForm/MgdbForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SwitchGiftDataManager.WinForm
+{
+    partial class MgdbForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MgdbForm));
+            lblMessage = new Label();
+            SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(94, 9);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(190, 20);
+            lblMessage.TabIndex = 0;
+            lblMessage.Text = "Downloading, please wait...";
+            // 
+            // MgdbForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(392, 37);
+            ControlBox = false;
+            Controls.Add(lblMessage);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MgdbForm";
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.Manual;
+            Text = "Mystery Gift Database";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblMessage;
+    }
+}
---
+++ SwitchGiftDataManager.WinForm/MgdbForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchGiftDataManager.WinForm;
+
+public partial class MgdbForm : Form
+{
+    public MgdbForm()
+    {
+        InitializeComponent();
+        this.Shown += (s, e) => Task.Run(async () => await DownloadRepoMGDB()).Wait();
+    }
+
+    private async Task DownloadRepoMGDB()
+    {
+        var url = "https://github.com/projectpokemon/EventsGallery/archive/refs/heads/master.zip";
+        using (var client = new HttpClient())
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "tmp");
+            var mgdbPath = Path.Combine(Environment.CurrentDirectory, "mgdb");
+
+            //Delete old residual files if exist
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+
+            if (Directory.Exists(mgdbPath))
+                Directory.Delete(mgdbPath, true);
+
+            Directory.CreateDirectory(path);
+
+            var zipPath = Path.Combine(path, "tmp.zip");
+            using (var response = await client.GetAsync(url))
+            {
+                using (var content = await response.Content.ReadAsStreamAsync())
+                {
+                    using (var stream = new FileStream(zipPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await content.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            ZipFile.ExtractToDirectory(zipPath, path);
+            File.Delete(zipPath);
+
+            var tmpMgbdPath = Path.Combine(path, "EventsGallery-master");
+
+            File.Delete(Path.Combine(tmpMgbdPath, ".gitignore"));
+            Directory.Delete(Path.Combine(tmpMgbdPath, "Extras"), true);
+            Directory.Delete(Path.Combine(tmpMgbdPath, "Unreleased"), true);
+            Directory.Delete(Path.Combine(tmpMgbdPath, "PKHeX Legality"), true);
+
+            var genPath = Path.Combine(tmpMgbdPath, "Released");
+            Directory.Delete(Path.Combine(genPath, "Gen 1"), true);
+            Directory.Delete(Path.Combine(genPath, "Gen 2"), true);
+            Directory.Delete(Path.Combine(genPath, "Gen 3"), true);
+            Directory.Delete(Path.Combine(genPath, "Gen 4"), true);
+            Directory.Delete(Path.Combine(genPath, "Gen 5"), true);
+            Directory.Delete(Path.Combine(genPath, "Gen 6"), true);
+
+            var gen7Path = Path.Combine(genPath, "Gen 7");
+            Directory.Delete(Path.Combine(gen7Path, "3DS"), true);
+            Directory.Delete(Path.Combine(Path.Combine(gen7Path, "Switch"), "Wondercard Records"), true);
+
+            var swshPath = Path.Combine(Path.Combine(genPath, "Gen 8"), "SwSh");
+            Directory.Delete(Path.Combine(swshPath, "Wild Area Events"), true);
+
+            var gen9Path = Path.Combine(genPath, "Gen 9");
+            Directory.Delete(Path.Combine(gen9Path, "Raid Events"), true);
+            Directory.Delete(Path.Combine(gen9Path, "Outbreak Events"), true);
+
+            Directory.Move(tmpMgbdPath, mgdbPath);
+            Directory.Delete(path, true);
+
+            this.FormClosed += (s, e) => MessageBox.Show($"The Mystery Gift Database has been downloaded in {mgdbPath}", "MGDB",
+                MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+
+            this.Close();
+        }
+    }
+}
